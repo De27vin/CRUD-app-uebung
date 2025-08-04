a64c77e8731ae1676fe267233c2137856a7de4d4
Devin de Spindler <devin.vipxxx@gmail.com>
2025-08-04 11:47:02 +0200

    Add: Set up UserController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,18 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>hallo</h1>
+    <div style="border: 3px solid black;">
+        <div style="margin: 10px;">
+            <h1>Registrierung</h1>
+            <form action="/register" method="POST">
+                @csrf
+                <input type="text" placeholder="Email">
+                <input type="password" placeholder="Passwort">
+                <br>
+                <button style="margin-top: 10px">Registrieren</button>
+            </form>
+            
+        </div>
+    </div>
 </body>
 </html>
