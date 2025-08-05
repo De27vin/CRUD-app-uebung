32da892a40980ccddad2f08578137531a3b8d584
Devin de Spindler <devin.vipxxx@gmail.com>
2025-08-05 17:17:43 +0200

    Add: CSS styling

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -1,18 +1,21 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Post bearbeiten</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
-    <h1>Post bearbeiten</h1>
-    <form action="/edit-post/{{$post->id}}" method="POST">
-        @csrf
-        @method('PUT')
-        <input type="text" name="title" value="{{$post->title}}">
-        <textarea name="body">{{$post->body}}</textarea>
-        <button>Änderungen speichern</button>
-    </form>
+    <div class="box">
+        <h1>Post bearbeiten</h1>
+        <form action="/edit-post/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="text" name="title" value="{{ $post->title }}">
+            <textarea name="body">{{ $post->body }}</textarea>
+            <button>Änderungen speichern</button>
+        </form>
+    </div>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,69 @@
-<html lang="en">
+<!DOCTYPE html>
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Post App</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 </head>
 <body>
 
     @auth
-    <h1>Homepage</h1>
-    <form action="/logout" method="POST">
-        @csrf
-        <button>Log out</button>
-    </form>
+        <h1>Homepage</h1>
 
-    <div style="border: 3px solid black;">
-        <h1>Neuen Post erstellen</h1>
-        <form action="/create-post" method="POST">
+        <form action="/logout" method="POST">
             @csrf
-            <input type="text" name="title" placeholder="Titel">
-            <textarea name="body" placeholder="Inhalt"></textarea>
-            <button>Post speichern!</button>
+            <button>Log out</button>
         </form>
-    </div>
 
-    <div style="border: 3px solid black;">
-        <h1>Posts</h1>
-        @foreach($posts as $post)
-        <div style="background-color: gray">
-            <h3>{{$post['title']}}</h3>
-            {{$post['body']}}
-
-            <p><a href="/edit-post/{{$post->id}}">Bearbeiten</a></p>
-
-            <form action="/delete-post/{{$post->id}}" method="POST">
+        <div class="box">
+            <h1>Neuen Post erstellen</h1>
+            <form action="/create-post" method="POST">
                 @csrf
-                @method('DELETE')
-                <button>Löschen</button>
+                <input type="text" name="title" placeholder="Titel">
+                <textarea name="body" placeholder="Inhalt"></textarea>
+                <button>Post speichern!</button>
             </form>
         </div>
-        @endforeach
-    </div>
 
+        <div class="box">
+            <h1>Posts</h1>
+            @foreach($posts as $post)
+                <div class="post">
+                    <h3>{{ $post->title }}</h3>
+                    <p>{{ $post->body }}</p>
+
+                    <p><a href="/edit-post/{{ $post->id }}">Bearbeiten</a></p>
+
+                    <form action="/delete-post/{{ $post->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button>Löschen</button>
+                    </form>
+                </div>
+            @endforeach
+        </div>
     @else
-    <div style="border: 3px solid black;">
-        <div style="margin: 10px;">
+        <div class="box">
             <h1>Anmelden</h1>
             <form action="/login" method="POST">
                 @csrf
                 <input name="loginemail" type="text" placeholder="Email">
                 <input name="loginpassword" type="password" placeholder="Passwort">
-                <br>
-                <button style="margin-top: 10px">Anmelden</button>
+                <button>Anmelden</button>
             </form>
-
         </div>
-    </div>
 
-    <div style="border: 3px solid black;">
-        <div style="margin: 10px;">
+        <div class="box">
             <h1>Registrierung</h1>
             <form action="/register" method="POST">
                 @csrf
                 <input name="email" type="text" placeholder="Email">
                 <input name="password" type="password" placeholder="Passwort">
-                <br>
-                <button style="margin-top: 10px">Registrieren</button>
+                <button>Registrieren</button>
             </form>
         </div>
-    </div>
-
-
     @endauth
-
-
 
 </body>
 </html>
